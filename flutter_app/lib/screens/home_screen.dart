c4c6a2c1e4c4eba8d9f08b5bf9d64bed7b35140a
patrick <patrickchow0803@gmail.com>
2020-04-03 20:01:05 -0400

    Added Transaction model and created a list of them.

--- flutter_app/lib/screens/home_screen.dart
+++ flutter_app/lib/screens/home_screen.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/transaction.dart';
 
 class HomeScreen extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

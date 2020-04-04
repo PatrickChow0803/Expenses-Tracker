f1739de8d11fd91148a7778e16c2512e3fafc31e
patrick <patrickchow0803@gmail.com>
2020-04-04 14:31:24 -0400

    Added Icon button to the appbar and a fab on the bottom

--- flutter_app/lib/screens/home_screen.dart
+++ flutter_app/lib/screens/home_screen.dart
@@ -19,64 +19,77 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // Gets rid of the bottom overflow problem
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: Text('Flutter App'),
-        ),
-        body: Column(
+      // Gets rid of the bottom overflow problem
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
+        title: Text('Flutter App'),
+      ),
+      body: Column(
 //          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blueAccent,
-                child: Text('Chart Placeholder'),
-              ),
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blueAccent,
+              child: Text('Chart Placeholder'),
             ),
-            Card(
-              child: Container(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: titleController,
+          ),
+          Card(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    controller: titleController,
 //                      onChanged: (value) {
 //                        titleInput = value;
 //                      },
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                      keyboardType: TextInputType.number,
-                      controller: amountController,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                    keyboardType: TextInputType.number,
+                    controller: amountController,
 //                      onChanged: (value) => amountInput = value,
+                  ),
+
+                  // Add transaction button
+                  Container(
+                    margin: EdgeInsets.only(top: 16.0),
+                    child: FlatButton(
+                      child: Text('Add Transaction'),
+                      textColor: Colors.purple,
+                      onPressed: () {
+                        Provider.of<TransactionData>(context, listen: false)
+                            .addTransaction(titleController.text,
+                                double.parse(amountController.text));
+
+                        // Gets rid of the keyboard when pressing the button
+                        FocusScope.of(context).unfocus();
+                      },
                     ),
-
-                    // Add transaction button
-                    Container(
-                      margin: EdgeInsets.only(top: 16.0),
-                      child: FlatButton(
-                        child: Text('Add Transaction'),
-                        textColor: Colors.purple,
-                        onPressed: () {
-                          Provider.of<TransactionData>(context, listen: false)
-                              .addTransaction(titleController.text,
-                                  double.parse(amountController.text));
-
-                          // Gets rid of the keyboard when pressing the button
-                          FocusScope.of(context).unfocus();
-                        },
-                      ),
-                    )
-                  ],
-                ),
+                  )
+                ],
               ),
             ),
-            TransactionList(),
-          ],
-        ));
+          ),
+          TransactionList(),
+        ],
+      ),
+      // Places the FAB on the center bottom of the app.
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
   }
 }
